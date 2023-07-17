f6b4f4a9ea36c32fcb6891cd51f755ddb3475a2a
Viraj Mehta <virajm@cs.cmu.edu>
2023-07-17 12:55:24 -0400

    all set up for trainingi

--- train_jeopardy.sh
+++
@@ -1,1 +0,0 @@
-python -u train.py model=pythia28 datasets=[jeopardy] loss=sft exp_name=jeopardy_sft_pythia28 gradient_accumulation_steps=2 batch_size=64 eval_batch_size=32 trainer=FSDPTrainer sample_during_eval=false model.fsdp_policy_mp=bfloat16
