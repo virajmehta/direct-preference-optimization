486a95b1e1a8c13e13544740f637ccca1414566f
Viraj Mehta <virajm@cs.cmu.edu>
2023-09-14 09:46:41 -0400

    WIP on new eval compare script

--- eval/new_eval_compare.py
+++ eval/new_eval_compare.py
@@ -24,6 +24,39 @@ def get_user_prompt(row, sample_1_name, sample_2_name):
     return user_prompt.format(prompt=prompt, sample1=sample1, sample2=sample2)
 
 
+def asof_merge_dfs(dfs, on, additional_merge_col):
+    """
+    Perform an asof merge on a list of dataframes followed by a regular merge on an additional column.
+    """
+    # Ensure all dataframes are sorted by the key
+    for i, df in enumerate(dfs):
+        dfs[i] = df.sort_values(by=on)
+
+    # Start with the first dataframe
+    merged_df = dfs[0]
+
+    # Iteratively merge each dataframe
+    for i, df in enumerate(dfs[1:]):
+        # Drop overlapping columns except for the merge keys
+        overlap_cols = [col for col in df.columns if col in merged_df.columns and col not in [on, additional_merge_col]]
+        df_dropped = df.drop(columns=overlap_cols)
+
+        # Ensure both DataFrames are sorted
+        merged_df = merged_df.sort_values(by=on)
+        df_dropped = df_dropped.sort_values(by=on)
+
+        breakpoint()
+        # Perform the asof merge
+        merged_df = pd.merge_asof(merged_df, df_dropped, on=on, direction='nearest', suffixes=('', f'_df{i+1}'))
+
+    breakpoint()
+    # After all asof merges are done, perform a final regular merge on 'prompt'
+    merged_df = pd.merge(merged_df, df, on=additional_merge_col, suffixes=('', f'_final'))
+
+    return merged_df
+
+
+
 def get_name(stem: str) -> str:
     return f"{stem}_sample"
 
@@ -33,25 +66,51 @@ def get_user_prompt_name(stem1: str, stem2: str) -> str:
 def get_decision_name(stem1: str, stem2: str) -> str:
     return f"{stem1}_vs_{stem2}_decision"
 
-def main(csv_paths: List[str]):
+def main(paths: List[str]):
     """
     Takes a list of paths that are all comparable (as in came from the same dataset and protocol).
 
     Creates a merged df with columns for the winners between each of the pairs. This should be suitable for learning curve generation downstream.
     """
-    csv_paths = [Path(p) for p in csv_paths]
-    dfs = [pd.read_csv(p) for p in csv_paths]
-    breakpoint()
+    csv_paths = []
+    existing_data = None
+    for p in paths:
+        p = Path(p)
+        if p.suffix == '.csv':
+            csv_paths.append(Path(p))
+        elif p.suffix == '.pkl':
+            with open(p, "rb") as f:
+                existing_data = pickle.load(f)
+        else:
+            logging.warning(f"Unexpected file type {p.suffix} for path {p}")
+    dfs = []
+    for csvp in csv_paths:
+        try:
+            dfs.append(pd.read_csv(csvp))
+        except Exception:
+            logging.warning(f"Failed to read csv {csvp}")
     stems = [p.stem for p in csv_paths]
-    dfs = [df.rename(columns={"sample": get_name(stem)}) for df, stem in zip(dfs, stems)]
+    existing_columns = [] if existing_data is None else existing_data.columns.tolist()
+    dfs = [df.rename(columns={"sample": get_name(stem)}) for df, stem in zip(dfs, stems) if get_name(stem) not in existing_columns]
+    # take the first 20 rows of each df for testing
+    # dfs = [df.iloc[:20] for df in dfs]
 
-    merged_df = reduce(lambda left, right: pd.merge(left, right, on=["prompt", "step"], how="inner"), dfs)
-    for stem_pair in tqdm(combinations(stems, 2)):
+    # merged_df = reduce(lambda left, right: pd.merge(left, right, on=["prompt", "step"], how="inner"), dfs)
+    merged_df = asof_merge_dfs(dfs, on="step", additional_merge_col="prompt")
+    if existing_data is not None:
+        merged_df = pd.merge(merged_df, existing_data, on=["prompt", "step"], how="inner")
+    all_stems = [colname[:-7] for colname in merged_df.columns.tolist() if colname.endswith("_sample")]
+    print(f"{len(merged_df)=}")
+    for stem_pair in tqdm(combinations(all_stems, 2)):
         print(f'Evaluating pair {stem_pair}')
         stem1, stem2 = stem_pair
         user_prompt_name = get_user_prompt_name(stem1, stem2)
+        if user_prompt_name in merged_df.columns:
+            logging.warning(f"Skipping pair {stem_pair} because it already exists")
+            continue
         merged_df[user_prompt_name] = merged_df.apply(lambda row: get_user_prompt(row, get_name(stem1), get_name(stem2)), axis=1)
-        completions = call_chats(zip(system_prompt_gen(), merged_df[user_prompt_name].tolist()))
+        breakpoint()
+        completions = call_chats(zip(system_prompt_gen(), merged_df[user_prompt_name].tolist()), temperature=0.1)
         vals = []
         for i, dec in enumerate(completions):
             if dec == 'A':
@@ -67,7 +126,7 @@ def main(csv_paths: List[str]):
         decision_name = get_decision_name(stem1, stem2)
         merged_df[decision_name] = vals
     pickle_name = ""
-    for stem in stems:
+    for stem in all_stems:
         pickle_name += f"{stem}-"
     pickle_name = pickle_name[:-1] + ".pkl"
     with open(pickle_name, "wb") as f:
