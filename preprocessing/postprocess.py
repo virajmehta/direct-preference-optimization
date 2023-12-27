0cc980ece66b74fbc597bc2df4225ebd282f0d5e
Viraj Mehta <virajm@cs.cmu.edu>
2023-12-27 14:53:20 -0600

    fixed preprocessing

--- preprocessing/postprocess.py
+++ preprocessing/postprocess.py
@@ -2,20 +2,17 @@ import pandas as pd
 import json
 import sys
 import re
+from sklearn.model_selection import train_test_split
 
 def extract_placeholder(data):
-    # Regular expression pattern to match both formats
     pattern = r'(?i)^(?:This joke is about )?(.*?)(?:\.\s*)?$'
-
-    # Searching for the pattern in the given data
     match = re.match(pattern, data)
     if match:
-        return match.group(1)  # Returns the captured group
+        return match.group(1)
     else:
-        return None  # No match found
+        return None
 
-
-def main(src, dst):
+def main(src, train_dst, test_dst):
     prompts = []
     responses = []
     with open(src, 'r') as f:
@@ -27,8 +24,16 @@ def main(src, dst):
             prompt = f"Tell me a joke about {about}."
             prompts.append(prompt)
             responses.append(joke)
+
     data = pd.DataFrame({'prompt': prompts, 'response': responses})
-    data.to_csv(dst, index=False)
+
+    # Split the data into a training set and a test set (90/10 split)
+    train_data, test_data = train_test_split(data, test_size=0.1, random_state=42)
+
+    # Write the datasets to separate CSV files
+    train_data.to_csv(train_dst, index=False)
+    test_data.to_csv(test_dst, index=False)
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
+
